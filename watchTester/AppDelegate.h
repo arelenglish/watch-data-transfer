1ae71f736976666bafd1344339d9fb6c3ce0705f
Arel English <arelenglish@gmail.com>
2016-01-19 10:50:13 -0500

    Initial Commit

---
+++ watchTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  watchTester
+//
+//  Created by Arel English on 1/19/16.
+//  Copyright © 2016 Videri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ watchTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  watchTester
+//
+//  Created by Arel English on 1/19/16.
+//  Copyright © 2016 Videri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
